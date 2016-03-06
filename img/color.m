fa579665e4bae1c2682e55ebdff7468beebfc8b5
dilhan119 <dilhanhettiarachchi@gmail.com>
2016-03-06 10:56:41 +0530

    Added files via upload

---
+++ img/color.m
@@ -0,0 +1,4 @@
+function image = color(im)
+
+im = max(im-100,0);
+image = im;
---
+++ img/gui.m
@@ -0,0 +1,116 @@
+function varargout = gui(varargin)
+% GUI MATLAB code for gui.fig
+%      GUI, by itself, creates a new GUI or raises the existing
+%      singleton*.
+%
+%      H = GUI returns the handle to a new GUI or the handle to
+%      the existing singleton*.
+%
+%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI.M with the given input arguments.
+%
+%      GUI('Property','Value',...) creates a new GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui
+
+% Last Modified by GUIDE v2.5 27-Feb-2016 16:59:14
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before gui is made visible.
+function gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui (see VARARGIN)
+
+% Choose default command line output for gui
+handles.output = hObject;
+handles.im = NaN;
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in browse.
+function browse_Callback(hObject, eventdata, handles)
+% hObject    handle to browse (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[FileName1,PathName1] = uigetfile({'*.gif';'*.jpg';'*.tif';'*.png'},'Select the US image for fusion');
+name =    strcat(PathName1,FileName1);
+image = imread(name);
+handles.im = image;
+axes(handles.axes);
+imshow(image);
+set(handles.process,'Enable','on');
+guidata(hObject, handles);
+
+% --- Executes on button press in process.
+function process_Callback(hObject, eventdata, handles)
+% hObject    handle to process (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+image = handles.im;
+
+g_image = rgb2gray(image);
+axes(handles.gray);
+imshow(g_image);
+
+h = fspecial('average');
+f_image = imfilter(g_image,h,'symmetric');
+axes(handles.filter);
+imshow(f_image);
+
+BW = im2bw(f_image,graythresh(f_image));
+BW = ~BW;
+BW = imfill(BW,'holes');
+axes(handles.thresh);
+imshow(BW);
+
+BW2 = edge(BW,'canny');
+axes(handles.edge);
+imshow(BW2);
+
+guidata(hObject, handles);
